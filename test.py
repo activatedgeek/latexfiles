80ac59a0bd09a435245e36a5a8b2fe4d94c145ef
Sanyam Kapoor <im@perhapsbay.es>
2021-04-22 15:13:45 +0530

    remove painful svg support

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-def random():
-  return 42
+def fibonacci(n):
+  if n < 0:
+    raise ValueError
+
+  if n == 0:
+    return 0
+  if n <= 2:
+    return 1
+
+  return fibonacci(n - 1) + fibonacci(n - 2)
